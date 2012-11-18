b802435d074212b9e58c590dd458435c79b972f8
unknown <D:\MAIL>
2012-11-18 21:55:55 +0600

    improved 'example_build_from_cmdline.dart'

--- example/example_build_from_cmdline.dart
+++ example/example_build_from_cmdline.dart
@@ -1,42 +1,84 @@
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:mirrors';
 
 main() {
-  var project = '../example/sample_extension.yaml';
-  var prjPath = new Path.fromNative(project);
-  var curDir = Utils.getRootScriptDirectory();
-  var curDirPath = new Path.fromNative(curDir);
-  var filepath = curDirPath.join(prjPath).toNativePath();
-  runAsDartScript(filepath);
-}
-
-void runAsDartScript(String project) {
+  var projectPath = Utils.toAbsolutePath('../example/sample_extension.yaml');
   var env = 'CCOMPILE';
   var ccompile = 'ccompile.dart';
-  var path = Utils.findFileInPathEnv(ccompile);
-  if(path.isEmpty) {
-    path = Utils.findFileInEnv(env, ccompile);
+  var script = Utils.findFile(env, ccompile);
+  if(script.isEmpty) {
+    Utils.errorFileNotFound(env, ccompile);
   }
 
-  if(path.isEmpty) {
-    Utils.writeString('Can not find $ccompile either in env["PATH"] nor env["${env}"]', stderr);
-    return;
-  }
-
-  var arguments = [path, project];
-  Utils.writeString('Building project...', stdout);
-  Process.run('dart', arguments).then((ProcessResult result) {
-    if(result.exitCode == 0) {
-      Utils.writeString('Building complete successfully.', stdout);
-    } else {
-      Utils.writeString('Building complete with some errors.', stdout);
-      Utils.writeString(result.stdout, stdout);
-      Utils.writeString(result.stderr, stderr);
-    }
-  });
+  Utils.runDartScript([script, projectPath],{
+    'start': 'Building project "$projectPath"',
+    'success': 'Building complete successfully',
+    'error': 'Building complete with some errors'})
+    .then((exitCode) {}); // Nothing
 }
 
 class Utils {
+  static Future<int> runDartScript(List arguments, Map messages) {
+    return new Future.immediate(null).chain((_) {
+      var dart = findDartVM();
+      var message = messages['start'];
+      if(!message.isEmpty) {
+        Utils.writeString(message, stdout);
+      }
+
+      return Process.run(dart, arguments).chain((ProcessResult result) {
+        if(result.exitCode == 0) {
+          var message = messages['success'];
+          if(!message.isEmpty) {
+            Utils.writeString(message, stdout);
+          }
+        } else {
+          var message = messages['error'];
+          if(!message.isEmpty) {
+            Utils.writeString(message, stdout);
+          }
+
+          Utils.writeString(result.stdout, stdout);
+          Utils.writeString(result.stderr, stderr);
+        }
+
+        return new Future.immediate(result.exitCode == 0 ? 0 : -1);
+      });
+    });
+  }
+
+  static String findDartVM() {
+    var filename;
+    if(Platform.operatingSystem == 'windows') {
+      filename = 'dart.exe';
+    } else {
+      filename = 'dart';
+    }
+
+    var env = 'DART_SDK';
+    var dart = findFile(env, filename, 'bin');
+    if(!dart.isEmpty) {
+      return dart;
+    }
+
+    errorFileNotFound(env, filename);
+  }
+
+  static void errorFileNotFound(String env, String filename) {
+    writeString('Error: Cannot find "$filename" either in env["PATH"] nor in env["${env}"]', stderr);
+    exit(-1);
+  }
+
+  static String findFile(String env, String filename, [String subdir]) {
+    var path = findFileInPathEnv(filename);
+    if(!path.isEmpty) {
+      return path;
+    }
+
+    return findFileInEnv(env, filename, subdir);
+  }
+
   static String findFileInPathEnv(String filename) {
     var separator = Platform.operatingSystem == 'windows' ? ';' : ':';
     var envPath = Platform.environment['PATH'];
@@ -54,14 +96,22 @@ class Utils {
     return '';
   }
 
-  static String findFileInEnv(String env, String filename) {
+  static String findFileInEnv(String env, String filename, [String subdir]) {
+    if(env == null || env.isEmpty) {
+      return '';
+    }
+
     var path = Platform.environment[env];
     if(path == null) {
       return '';
     }
 
-    path = new Path.fromNative('$path').append(filename).toNativePath();
-    path = expandEnvironmentVars([path])[0];
+    path = new Path.fromNative('$path');
+    if(subdir != null) {
+      path = path.append(subdir);
+    }
+
+    path = path.append(filename).toNativePath();
     if(new File(path).existsSync()) {
       return path;
     }
@@ -69,26 +119,11 @@ class Utils {
     return '';
   }
 
-  static List<String> expandEnvironmentVars(List<String> strings) {
-    var list = [];
-    var len = strings.length;
-    for(var string in strings) {
-      list.add(_expandMacro(string, (s) => Platform.environment[s]));
-    }
-
-    return list;
-  }
-
-  static String _expandMacro(String string, String callback(String)) {
-    RegExp exp = const RegExp(r'(%\S+?%)');
-    var matches = exp.allMatches(string);
-    for(var match in matches) {
-      var seq = match.group(0);
-      var key = seq.substring(1, seq.length - 1);
-      string = string.replaceAll(seq, callback(key));
-    }
-
-    return string;
+  static String toAbsolutePath(String path) {
+    var projectPath = new Path.fromNative(path);
+    var curDir = Utils.getRootScriptDirectory();
+    var curDirPath = new Path.fromNative(curDir);
+    return curDirPath.join(projectPath).toNativePath();
   }
 
   static String newline = Platform.operatingSystem == 'windows' ? '\r\n' : '\n';
